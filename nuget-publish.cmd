bbd7a5d91b337eb127e20cb2723fef7978a6b6bb
Ivan Kochurkin <kvanttt@gmail.com>
2021-01-08 17:04:58 +0300

    Remove YandexLinguistics.NET.Gui since it's not actual and not crossplatform

--- nuget-publish.cmd
+++
@@ -1,4 +0,0 @@
-@cd YandexLinguistics.NET
-..\.nuget\nuget.exe pack YandexLinguistics.NET.csproj -Version 1.1 -Prop Configuration=Release -OutputDirectory bin
-..\.nuget\nuget.exe push bin\YandexLinguistics.NET.1.1.nupkg -ApiKey
-@pause
